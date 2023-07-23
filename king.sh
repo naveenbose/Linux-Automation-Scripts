6c6f89a1e7cd1b1109ea1be34802fe68ae28cfc0
naveenbose <121077165+naveenbose@users.noreply.github.com>
2023-07-23 16:00:33 +0530

    Update king.sh

--- king.sh
+++ king.sh
@@ -3,7 +3,7 @@
 #Author: Naveen Bose
 #Description: This script is useful while playing CTFs, but platform like tryhackme.com forbids this kind of automated scripts to add your name into king.txt, But im curious what if i create this script so i did it for fun. Tested localy not in CTFs environment.
 #########################################################
-
+# Check if the correct number of arguments is provided
 if [ $# -ne 1 ];
 then
     echo "Append filename you want to create: $0 <filename>"
@@ -12,11 +12,12 @@ fi
 
 
 filename=$1
-
+#Check if the file is exist and it has write permissions
 if [ -f "$filename" ] && [ -w "$filename"  ]
 then
     echo "N4V33N" > $filename
 else
+#Create file if doesn't in the correct path 
     touch "/root/$filename"
     echo "N4V33N" > "/root/$filename"
 fi
